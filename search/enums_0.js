79d90dd9f093f93ad4a515deee434c0e33732d30
John McNamara <jmcnamara@cpan.org>
2016-05-30 22:58:15 +0100

    Updated docs.

--- search/enums_0.js
+++ search/enums_0.js
@@ -3,12 +3,11 @@ var searchData=
   ['lxw_5fboolean',['lxw_boolean',['../common_8h.html#a57bffaf0ff3cb4e9f4f7f2b0b6dad349',1,'common.h']]],
   ['lxw_5fchart_5ftypes',['lxw_chart_types',['../chart_8h.html#a13ff0553ea84ef3ef3677be48421a2d2',1,'chart.h']]],
   ['lxw_5fdefined_5fcolors',['lxw_defined_colors',['../format_8h.html#abeafc6e8222ba9fcfd4f2c2f4e9ea38b',1,'format.h']]],
+  ['lxw_5ferror',['lxw_error',['../common_8h.html#a04b59aa3c578bc75f88ac16a979c5fe6',1,'common.h']]],
   ['lxw_5fformat_5falignments',['lxw_format_alignments',['../format_8h.html#adeeb0d70c7c873422d642323e7e1ecf5',1,'format.h']]],
   ['lxw_5fformat_5fborders',['lxw_format_borders',['../format_8h.html#a4d5e924b1acd9571fd5e0fe2615e9f8f',1,'format.h']]],
   ['lxw_5fformat_5fpatterns',['lxw_format_patterns',['../format_8h.html#a40e28394b961e69d4bee27579b4476e7',1,'format.h']]],
   ['lxw_5fformat_5fscripts',['lxw_format_scripts',['../format_8h.html#a0ff8fd761893f60fddbd7f1cab4ac75a',1,'format.h']]],
   ['lxw_5fformat_5funderlines',['lxw_format_underlines',['../format_8h.html#af530d7971d15c164f3fb6bbfede9a39f',1,'format.h']]],
-  ['lxw_5fgridlines',['lxw_gridlines',['../worksheet_8h.html#a487e0bffad0a348421b825d715621c20',1,'worksheet.h']]],
-  ['lxw_5fworkbook_5ferror',['lxw_workbook_error',['../workbook_8h.html#a7272e60b9a9bb05593b1c21a6ec2f5df',1,'workbook.h']]],
-  ['lxw_5fworksheet_5ferror',['lxw_worksheet_error',['../worksheet_8h.html#a7691f23e402e9db78212ec316e055b89',1,'worksheet.h']]]
+  ['lxw_5fgridlines',['lxw_gridlines',['../worksheet_8h.html#a487e0bffad0a348421b825d715621c20',1,'worksheet.h']]]
 ];
--- search/search.js
+++ search/search.js
@@ -10,7 +10,7 @@ var indexSectionsWithContent =
   0: "acdefghiklmnoprstuwxy",
   1: "l",
   2: "cfuw",
-  3: "cfw",
+  3: "cflw",
   4: "acdfhikmnopstxy",
   5: "l",
   6: "l",
