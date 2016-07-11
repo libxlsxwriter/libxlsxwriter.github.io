e355639a73d0323be63bab686c1c63282f2c1358
John McNamara <jmcnamara@cpan.org>
2016-07-11 23:51:46 +0100

    Updated docs.

--- search/variables_c.js
+++ search/variables_c.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['title',['title',['../structlxw__doc__properties.html#a23d8fd5995ef26d1eab54c4dbd92393c',1,'lxw_doc_properties']]]
+  ['title',['title',['../structlxw__doc__properties.html#a23d8fd5995ef26d1eab54c4dbd92393c',1,'lxw_doc_properties']]],
+  ['tmpdir',['tmpdir',['../structlxw__workbook__options.html#a6a79cfd2824acc5a438f37d4f79da325',1,'lxw_workbook_options']]]
 ];
