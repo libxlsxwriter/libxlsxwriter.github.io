ffce1964466c7de530bd65905c47f8bdf380897f
John McNamara <jmcnamara@cpan.org>
2017-01-11 00:12:09 +0000

    Updated docs.

--- search/functions_0.js
+++ search/functions_0.js
@@ -7,6 +7,8 @@ var searchData=
   ['chart_5faxis_5fminor_5fgridlines_5fset_5fvisible',['chart_axis_minor_gridlines_set_visible',['../chart_8h.html#aa53081409c448c71a535bd5dd1c0cfeb',1,'chart.h']]],
   ['chart_5faxis_5foff',['chart_axis_off',['../chart_8h.html#a5b982a99851997f9b090aaf0c1dfbd74',1,'chart.h']]],
   ['chart_5faxis_5fset_5ffill',['chart_axis_set_fill',['../chart_8h.html#ade5392d9d303458c48420f89cc3a8b5e',1,'chart.h']]],
+  ['chart_5faxis_5fset_5finterval_5ftick',['chart_axis_set_interval_tick',['../chart_8h.html#accdb835432241836d6a8dd36c98246ab',1,'chart.h']]],
+  ['chart_5faxis_5fset_5finterval_5funit',['chart_axis_set_interval_unit',['../chart_8h.html#ad92a8a1ad8c6225c05f36287510fa0ea',1,'chart.h']]],
   ['chart_5faxis_5fset_5flabel_5fposition',['chart_axis_set_label_position',['../chart_8h.html#a389ffe61edd2347a86282ae634eaf5d0',1,'chart.h']]],
   ['chart_5faxis_5fset_5fline',['chart_axis_set_line',['../chart_8h.html#ab7c72e968f26df7184878e58d7324960',1,'chart.h']]],
   ['chart_5faxis_5fset_5flog_5fbase',['chart_axis_set_log_base',['../chart_8h.html#ac27a9a20daf4f0a6be8595fbd7f41151',1,'chart.h']]],
