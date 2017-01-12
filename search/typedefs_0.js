a7a9412ec99067c41c790eb7d0fa9fa8e01327cb
John McNamara <jmcnamara@cpan.org>
2017-01-12 23:58:10 +0000

    Updated docs.

--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -14,6 +14,7 @@ var searchData=
   ['lxw_5fchart_5fpattern',['lxw_chart_pattern',['../chart_8h.html#af24ae0c4e184d1f94895ec62439dabab',1,'chart.h']]],
   ['lxw_5fchart_5fpattern_5ftype',['lxw_chart_pattern_type',['../chart_8h.html#af4881c046f6e6b9207c14e1da970ef84',1,'chart.h']]],
   ['lxw_5fchart_5fseries',['lxw_chart_series',['../chart_8h.html#a3a404c5dd6bc6d21a5e20c2e96ccb701',1,'chart.h']]],
+  ['lxw_5fchart_5ftick_5fmark',['lxw_chart_tick_mark',['../chart_8h.html#a774fe4039f890235d9bd199ca607a8ad',1,'chart.h']]],
   ['lxw_5fchart_5ftype',['lxw_chart_type',['../chart_8h.html#ad30e22b3cf06dddd125254b2399719f3',1,'chart.h']]],
   ['lxw_5fcol_5ft',['lxw_col_t',['../common_8h.html#a4fedd8dede664b070e84e850a3f92fd2',1,'common.h']]],
   ['lxw_5fcolor_5ft',['lxw_color_t',['../format_8h.html#a422d719f90588b17d1399eaced80ae31',1,'format.h']]],
