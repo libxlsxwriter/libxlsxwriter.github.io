257371d05689a1b8330cb42ad93f2d50a2732b9d
John McNamara <jmcnamara@cpan.org>
2018-03-18 00:59:33 +0000

    Updated docs.

--- search/all_14.js
+++ search/all_14.js
@@ -21,6 +21,7 @@ var searchData=
   ['working_20with_20data_20validation',['Working with Data Validation',['../working_with_data_validation.html',1,'']]],
   ['working_20with_20dates_20and_20times',['Working with Dates and Times',['../working_with_dates.html',1,'']]],
   ['working_20with_20formats',['Working with Formats',['../working_with_formats.html',1,'']]],
+  ['working_20with_20formulas',['Working with Formulas',['../working_with_formulas.html',1,'']]],
   ['working_20with_20memory_20and_20performance',['Working with Memory and Performance',['../working_with_memory.html',1,'']]],
   ['working_20with_20outlines_20and_20grouping',['Working with Outlines and Grouping',['../working_with_outlines.html',1,'']]],
   ['worksheet_2eh',['worksheet.h',['../worksheet_8h.html',1,'']]],
