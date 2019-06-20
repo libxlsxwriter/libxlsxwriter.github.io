a44e6eb11cb53e82cd8b80b296c9c9669fc3acf4
John McNamara <jmcnamara@cpan.org>
2019-06-21 00:08:36 +0100

    Updated docs.

--- search/all_12.js
+++ search/all_12.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['underline',['underline',['../structlxw__chart__font.html#a13fce73724d63f5f2ab70a1abf64f758',1,'lxw_chart_font']]],
+  ['use_5fzip64',['use_zip64',['../structlxw__workbook__options.html#aeff094d01eb078b6621bd3ebbd6e326b',1,'lxw_workbook_options']]],
   ['utility_2eh',['utility.h',['../utility_8h.html',1,'']]]
 ];
--- search/pages_a.js
+++ search/pages_a.js
@@ -6,6 +6,7 @@ var searchData=
   ['working_20with_20dates_20and_20times',['Working with Dates and Times',['../working_with_dates.html',1,'']]],
   ['working_20with_20formats',['Working with Formats',['../working_with_formats.html',1,'']]],
   ['working_20with_20formulas',['Working with Formulas',['../working_with_formulas.html',1,'']]],
+  ['working_20with_20vba_20macros',['Working with VBA Macros',['../working_with_macros.html',1,'']]],
   ['working_20with_20memory_20and_20performance',['Working with Memory and Performance',['../working_with_memory.html',1,'']]],
   ['working_20with_20outlines_20and_20grouping',['Working with Outlines and Grouping',['../working_with_outlines.html',1,'']]]
 ];
