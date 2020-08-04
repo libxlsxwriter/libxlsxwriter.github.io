d1cea0207057038f26a351d93460c9e52bbd3512
John McNamara <jmcnamara@cpan.org>
2020-08-04 11:52:10 +0100

    Updated docs.

--- search/variables_3.js
+++ search/variables_3.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['dash_5ftype',['dash_type',['../structlxw__chart__line.html#a0f3901f6d5faa3a8fa8057a56ded89de',1,'lxw_chart_line']]],
   ['day',['day',['../structlxw__datetime.html#ad6bcea90e5ed9cbba682e065ace4a7be',1,'lxw_datetime']]],
-  ['delete',['delete',['../structlxw__chart__data__label.html#a46ecdef66d09a889808fe03fe3d06c18',1,'lxw_chart_data_label']]],
   ['delete_5fcolumns',['delete_columns',['../structlxw__protection.html#aae0654e82d7404e1dc05e7ebd7ea88e4',1,'lxw_protection']]],
   ['delete_5frows',['delete_rows',['../structlxw__protection.html#a89fa399211fa69eccda37cb5619a2bc5',1,'lxw_protection']]],
   ['description',['description',['../structlxw__image__options.html#acd2ff64da3a930c2b7715ad71fae8d39',1,'lxw_image_options']]],
