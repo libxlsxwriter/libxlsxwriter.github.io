1fbe8bc26b07e8094f564d1797fdd4da70a7d83a
John McNamara <jmcnamara@cpan.org>
2025-07-01 00:13:21 +0100

    Updated docs.

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,8 +1,10 @@
 var searchData=
 [
-  ['lxw_5fdatetime_5fto_5fexcel_5fdatetime_0',['lxw_datetime_to_excel_datetime',['../utility_8h.html#ac58c9e5aa239b938ef78c10c055c0f79',1,'utility.h']]],
-  ['lxw_5fstrerror_1',['lxw_strerror',['../utility_8h.html#a49d13e670997998741d46063bdc484a9',1,'utility.h']]],
-  ['lxw_5funixtime_5fto_5fexcel_5fdate_2',['lxw_unixtime_to_excel_date',['../utility_8h.html#aa27dba0457269a1044e14fa8dc9294d2',1,'utility.h']]],
-  ['lxw_5fversion_3',['lxw_version',['../utility_8h.html#a03ba9ddfcf3038f2a977bfbf830ed8f7',1,'utility.h']]],
-  ['lxw_5fversion_5fid_4',['lxw_version_id',['../utility_8h.html#a1be3ef9e966d0a54fcddea06583232b4',1,'utility.h']]]
+  ['lxw_5fdatetime_5fto_5fexcel_5fdate_5fwith_5fepoch_0',['lxw_datetime_to_excel_date_with_epoch',['../utility_8h.html#a1352cea39063f569f68c6b2774dbe20a',1,'utility.h']]],
+  ['lxw_5fdatetime_5fto_5fexcel_5fdatetime_1',['lxw_datetime_to_excel_datetime',['../utility_8h.html#ac58c9e5aa239b938ef78c10c055c0f79',1,'utility.h']]],
+  ['lxw_5fstrerror_2',['lxw_strerror',['../utility_8h.html#a49d13e670997998741d46063bdc484a9',1,'utility.h']]],
+  ['lxw_5funixtime_5fto_5fexcel_5fdate_3',['lxw_unixtime_to_excel_date',['../utility_8h.html#aa27dba0457269a1044e14fa8dc9294d2',1,'utility.h']]],
+  ['lxw_5funixtime_5fto_5fexcel_5fdate_5fwith_5fepoch_4',['lxw_unixtime_to_excel_date_with_epoch',['../utility_8h.html#a8a3563aa8dd3691ecf458fe73ce91dce',1,'utility.h']]],
+  ['lxw_5fversion_5',['lxw_version',['../utility_8h.html#a03ba9ddfcf3038f2a977bfbf830ed8f7',1,'utility.h']]],
+  ['lxw_5fversion_5fid_6',['lxw_version_id',['../utility_8h.html#a1be3ef9e966d0a54fcddea06583232b4',1,'utility.h']]]
 ];
